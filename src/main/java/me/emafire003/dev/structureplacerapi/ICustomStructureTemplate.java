b3d9081dac3be835a702f83180bcc4501685be98
Emafire003 <29462910+Emafire003@users.noreply.github.com>
2026-02-03 14:58:27 +0100

    Replaced onlyAir thingy with a BlockTag based system, so now its possible to specify blocks with a tags that will be either kept or are going to be the only ones to be replaced. Added the methods to the StructurePlacerAPI class. Packed up the load methods, updated javadoc accordingly Updated readme with the new stuff
    (cherry picked from commit 30d046266d2f101b22dd9eacc46f0a4d2a50257f)

--- src/main/java/me/emafire003/dev/structureplacerapi/ICustomStructureTemplate.java
+++ src/main/java/me/emafire003/dev/structureplacerapi/ICustomStructureTemplate.java
@@ -1,10 +1,28 @@
 package me.emafire003.dev.structureplacerapi;
 
+import net.minecraft.block.Block;
+import net.minecraft.registry.tag.TagKey;
+
+@SuppressWarnings("unused")
 public interface ICustomStructureTemplate {
+    /** This is used to tell the StructureTemplate that we are using the StructurePlacerApi system */
     void structurePlacerAPI$setCustom(boolean custom);
     boolean structurePlacerAPI$isCustom();
+    /** Weather or not this structure should replace the bedrock */
     void structurePlacerAPI$setReplaceBedrock(boolean replace);
     boolean structurePlacerAPI$getReplaceBedrock();
-    void structurePlacerAPI$setOnlyReplaceAir(boolean replaceAir);
-    boolean structurePlacerAPI$getOnlyReplaceAir();
+    /** Weather or not this structure should replace the barrier block */
+    void structurePlacerAPI$setReplaceBarrier(boolean replace);
+    boolean structurePlacerAPI$getReplaceBarrier();
+    /** Weather or not this structure should only replace blocks with a certain tag, like Air blocks*/
+    void structurePlacerAPI$setOnlyReplaceTagBlocks(boolean replace, TagKey<Block> tag);
+    boolean structurePlacerAPI$getOnlyReplaceTagBlocks();
+    /** Weather or not this structure should replace the blocks with the specified tag */
+    void structurePlacerAPI$setPreventReplacementOfTagBlocks(boolean replace, TagKey<Block> tag);
+    boolean structurePlacerAPI$getPreventReplacementOfTagBlocks();
+    /** Sets the tag for the blocks needed for {@link #structurePlacerAPI$setPreventReplacementOfTagBlocks(boolean, TagKey)} or {@link #structurePlacerAPI$setOnlyReplaceTagBlocks(boolean, TagKey)}*/
+    void structurePlacerAPI$setTaggedBlocks(TagKey<Block> taggedBlocks);
+    TagKey<Block> structurePlacerAPI$getTaggedBlocks();
+    
+    
 }
