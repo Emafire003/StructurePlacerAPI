cbbb55b85bf2df33cb6b0bcc86ce963412620113
Emafire003 <29462910+Emafire003@users.noreply.github.com>
2026-02-24 14:30:21 +0100

    forge fixes

--- src/main/java/me/emafire003/dev/structureplacerapi/StructurePlacerAPIMod.java
+++ src/main/java/me/emafire003/dev/structureplacerapi/StructurePlacerAPIMod.java
@@ -1,12 +1,11 @@
 package me.emafire003.dev.structureplacerapi;
 
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod("structureplacerapi")
 public class StructurePlacerAPIMod {
-    public StructurePlacerAPIMod(IEventBus modBus) {
+    public StructurePlacerAPIMod() {
         MinecraftForge.EVENT_BUS.register(StructurePlacerAPI.class);
     }
 }
